16fe310e41d2927a4baf2701fc8e8248a7f7e3d1
alesharik <alesharikReserv@yandex.ru>
2017-02-18 23:00:49 +0300

    Starting package documentation

---
+++ api/src/com/alesharik/webserver/reporting/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains Reporting plugin api
+ *
+ * @see com.alesharik.webserver.reporting.Reporter
+ * @see com.alesharik.webserver.reporting.ReportingModule
+ * @see com.alesharik.webserver.reporting.ReportingModuleMXBean
+ */
+package com.alesharik.webserver.reporting;
---
+++ main/src/com/alesharik/webserver/generators/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains error page generators implementations
+ *
+ * @see com.alesharik.webserver.generators.BasicErrorPageConstructor
+ * @see com.alesharik.webserver.generators.FileBasedErrorPageConstructor
+ * @see com.alesharik.webserver.generators.ModularErrorPageGenerator
+ */
+package com.alesharik.webserver.generators;
