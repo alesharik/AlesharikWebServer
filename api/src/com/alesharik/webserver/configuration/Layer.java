2ef1b39e488f283edb8008bc2cc801ed95191941
alesharik <alesharikReserv@yandex.ru>
2017-04-06 17:20:26 +0300

    Module architecture updates

--- api/src/com/alesharik/webserver/configuration/Layer.java
+++ api/src/com/alesharik/webserver/configuration/Layer.java
@@ -1,9 +1,124 @@
 package com.alesharik.webserver.configuration;
 
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+/**
+ * Layer hold submodules and another layers
+ *
+ * @implNote Class must be thread-safe!
+ */
+@ThreadSafe
 public interface Layer {
-    SubModule[] getSubModules();
+    /**
+     * Return all Layer submodules
+     */
+    @Nonnull
+    List<SubModule> getSubModules();
 
-    Layer[] getSubLayers();
+    /**
+     * Return all sub layers
+     */
+    @Nonnull
+    List<Layer> getSubLayers();
 
+    /**
+     * Return layer name
+     */
+    @Nonnull
     String getName();
+
+    default void start() {
+        getSubModules().forEach(SubModule::start);
+        getSubLayers().forEach(Layer::start);
+    }
+
+    default void shutdown() {
+        getSubModules().forEach(SubModule::shutdown);
+        getSubLayers().forEach(Layer::shutdown);
+    }
+
+    default void shutdownNow() {
+        getSubModules().forEach(SubModule::shutdownNow);
+        getSubLayers().forEach(Layer::shutdownNow);
+    }
+
+    default void reload() {
+        getSubModules().forEach(SubModule::reload);
+        getSubLayers().forEach(Layer::reload);
+    }
+
+    /**
+     * Search for submodule only in this layer
+     *
+     * @param name submodule name
+     * @return Optional with submodule or empty
+     */
+    @Nonnull
+    default Optional<SubModule> getSubModuleByName(@Nonnull String name) {
+        for(SubModule subModule : getSubModules()) {
+            if(name.equals(subModule.getName())) {
+                return Optional.of(subModule);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Search for submodule in this layer and sub-layers
+     *
+     * @param name submodule name
+     * @return Optional with submodule or empty
+     */
+    @Nonnull
+    default Optional<SubModule> findSubModuleByName(@Nonnull String name) {
+        return Stream.concat(getSubModules().stream(), getSubLayers().stream()
+                .flatMap(new Function<Layer, Stream<? extends SubModule>>() {
+                    @Override
+                    public Stream<? extends SubModule> apply(Layer layer) {
+                        return Stream.concat(layer.getSubModules().stream(), layer.getSubLayers().stream().flatMap(this));
+                    }
+                }))
+                .filter(subModule -> name.equals(subModule.getName()))
+                .findAny();
+    }
+
+    /**
+     * Search for sub-layer in this layer
+     *
+     * @param name layer name
+     * @return Optional with layer or empty
+     */
+    @Nonnull
+    default Optional<Layer> getLayerByName(@Nonnull String name) {
+        for(Layer subLayer : getSubLayers()) {
+            if(name.equals(subLayer.getName())) {
+                return Optional.of(subLayer);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Search for sub-layer in this layer and sub-layers
+     *
+     * @param name layer name
+     * @return Optional with layer or empty
+     */
+    @Nonnull
+    default Optional<Layer> findLayerByName(@Nonnull String name) {
+        return getSubLayers().stream()
+                .flatMap(new Function<Layer, Stream<? extends Layer>>() {
+                    @Override
+                    public Stream<? extends Layer> apply(Layer layer) {
+                        return Stream.concat(Stream.of(layer), layer.getSubLayers().stream().flatMap(this));
+                    }
+                })
+                .filter(r -> name.equals(r.getName()))
+                .findAny();
+    }
 }
--- api/src/com/alesharik/webserver/configuration/SubModule.java
+++ api/src/com/alesharik/webserver/configuration/SubModule.java
@@ -1,15 +1,41 @@
 package com.alesharik.webserver.configuration;
 
-import com.alesharik.webserver.configuration.message.MessageManager;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
 
+/**
+ * SubModule is part of module. It is useful for split code into small independent pieces
+ *
+ * @implNote Implementation must be thread-safe
+ */
+@ThreadSafe
 public interface SubModule {
-    void setMessageManager(MessageManager manager);
-
+    /**
+     * Return submodule unique name
+     */
+    @Nonnull
     String getName();
 
+    /**
+     * Start submodule
+     */
     void start();
 
+    /**
+     * Shutdown submodule without waiting
+     */
     void shutdownNow();
 
-    void reload();
+    /**
+     * Shutdown submodule gracefully
+     */
+    void shutdown();
+
+    /**
+     * Reload submodule(if config changed, etc) - shutdown and start it
+     */
+    default void reload() {
+        shutdown();
+        start();
+    }
 }
