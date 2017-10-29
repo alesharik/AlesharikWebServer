f05e111859752c59cc4ad521638f3c3fed0c1712
alesharik <alesharikReserv@yandex.ru>
2017-10-29 22:05:22 +0300

    LoggerThreadCacheTest fix

--- api/test/com/alesharik/webserver/logger/LoggerThreadCacheTest.java
+++ api/test/com/alesharik/webserver/logger/LoggerThreadCacheTest.java
@@ -37,7 +37,7 @@ public class LoggerThreadCacheTest {
     @Test
     public void cacheClearTest() throws Exception {
         cache.add(String.class, "test");
-        Thread.sleep(Logger.LoggerThreadCache.CONTAINS_TIME + Logger.LoggerThreadCache.UPDATE_DELAY);
+        Thread.sleep(Logger.LoggerThreadCache.CONTAINS_TIME + Logger.LoggerThreadCache.UPDATE_DELAY + 10);
         assertNull(cache.get(String.class));
     }
 
