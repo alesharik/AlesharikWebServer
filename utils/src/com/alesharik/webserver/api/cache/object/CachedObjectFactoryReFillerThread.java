444276947b29055753c688d2c6c013e8d30ad3e4
alesharik <alesharikReserv@yandex.ru>
2017-12-29 11:44:28 +0300

    Move statistics and cache to utils module

--- utils/src/com/alesharik/webserver/api/cache/object/CachedObjectFactoryReFillerThread.java
+++ utils/src/com/alesharik/webserver/api/cache/object/CachedObjectFactoryReFillerThread.java
@@ -16,6 +16,24 @@
  *
  */
 
+/*
+ *  This file is part of AlesharikWebServer.
+ *
+ *     AlesharikWebServer is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     AlesharikWebServer is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with AlesharikWebServer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.alesharik.webserver.api.cache.object;
 
 import com.alesharik.webserver.internals.ShutdownState;
@@ -29,7 +47,7 @@ import java.util.concurrent.atomic.AtomicReference;
 /**
  * This thread refills all {@link CachedObjectFactory} with dead instances
  */
-class CachedObjectFactoryReFillerThread extends Thread {//TODO move to java.lang.ref.Cleaner in Java 9
+final class CachedObjectFactoryReFillerThread extends Thread {//TODO move to java.lang.ref.Cleaner in Java 9
     private static final ReferenceQueue<Object> queue = new ReferenceQueue<>();
     private static final List<Ref> refs = new CopyOnWriteArrayList<>();
     static final AtomicReference<CachedObjectFactoryReFillerThread> thread = new AtomicReference<>();
--- utils/src/com/alesharik/webserver/api/cache/object/FixedCachedObjectFactory.java
+++ utils/src/com/alesharik/webserver/api/cache/object/FixedCachedObjectFactory.java
@@ -16,6 +16,24 @@
  *
  */
 
+/*
+ *  This file is part of AlesharikWebServer.
+ *
+ *     AlesharikWebServer is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     AlesharikWebServer is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with AlesharikWebServer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.alesharik.webserver.api.cache.object;
 
 import javax.annotation.Nonnull;
@@ -23,7 +41,7 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.locks.StampedLock;
 
-public class FixedCachedObjectFactory<T extends Recyclable> implements CachedObjectFactory<T> {
+public final class FixedCachedObjectFactory<T extends Recyclable> implements CachedObjectFactory<T> {
     protected final StampedLock stampedLock;
     protected final int maxCount;
     protected final ObjectFactory<T> factory;
