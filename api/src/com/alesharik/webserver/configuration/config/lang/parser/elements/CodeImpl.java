ac82e943278bf2ef67f00a9ca5483d2048f8d7e8
alesharik <alesharikReserv@yandex.ru>
2018-04-27 19:27:56 +0300

    Add missing implementation

---
+++ api/src/com/alesharik/webserver/configuration/config/lang/parser/elements/CodeImpl.java
@@ -0,0 +1,31 @@
+/*
+ *  This file is part of AlesharikWebServer.
+ *
+ *     AlesharikWebServer is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     AlesharikWebServer is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with AlesharikWebServer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.alesharik.webserver.configuration.config.lang.parser.elements;
+
+import com.alesharik.webserver.configuration.config.lang.element.ConfigurationCodeElement;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public final class CodeImpl implements ConfigurationCodeElement {
+    private final String languageName;
+    private final String code;
+    private final String name;
+}
