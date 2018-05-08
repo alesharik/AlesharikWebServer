a0669b80d6fa8c047699078f6610e428e501cf42
alesharik <alesharikReserv@yandex.ru>
2018-05-08 21:14:23 +0300

    New Module API

---
+++ api/src/com/alesharik/webserver/configuration/module/Configuration.java
@@ -0,0 +1,35 @@
+/*
+ *  This file is part of AlesharikWebServer.
+ *
+ *     AlesharikWebServer is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     AlesharikWebServer is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with AlesharikWebServer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.alesharik.webserver.configuration.module;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotated type will be used as configuration object to fill all annotated fields
+ *
+ * @see ConfigurationValue
+ * @see LinkModule
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD})
+public @interface Configuration {
+}
---
+++ api/src/com/alesharik/webserver/configuration/module/Module.java
@@ -0,0 +1,59 @@
+/*
+ *  This file is part of AlesharikWebServer.
+ *
+ *     AlesharikWebServer is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     AlesharikWebServer is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with AlesharikWebServer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.alesharik.webserver.configuration.module;
+
+import javax.annotation.Nonnull;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation indicates that this class is a module.
+ * Modules are non-singleton {@link com.alesharik.webserver.base.bean.Bean}s with autowire. Every module ans it's parts
+ * will be run in it's own bean context
+ *
+ * @see Configuration
+ * @see Configure
+ * @see Shutdown
+ * @see Start
+ * @see ShutdownNow
+ * @see com.alesharik.webserver.configuration.module.layer.Layer
+ * @see com.alesharik.webserver.configuration.module.layer.Layered
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface Module {
+    /**
+     * Return module's name
+     *
+     * @return module's name
+     */
+    @Nonnull
+    String value();
+
+    /**
+     * Enable auto-invoke option. All children's lifecycle events will be invoked automatically
+     *
+     * @return is auto-invoke option enabled
+     */
+    boolean autoInvoke() default false;
+}
