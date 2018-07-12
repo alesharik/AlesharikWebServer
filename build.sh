92100cd7b59ce55da444d3257ce4dadbb3c51534
alesharik <alesharikReserv@yandex.ru>
2018-07-12 22:01:24 +0300

    Fix build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 apt-get update -qq
 apt-get install --allow-unauthenticated -qq curl openjdk-8-jdk gcc
-apt-get install --allow-unauthenticated -qq util-linux libblkid1 libblkid-dev systemd libdbus-1-dev libsystemd-dev
+apt-get install --allow-unauthenticated -qq util-linux libblkid1 libblkid-dev systemd libdbus-1-dev libsystemd-dev libclang-dev
 export CXX="clang++-3.4"
 
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain 1.27.0 -y
