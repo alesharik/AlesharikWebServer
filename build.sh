af170437e7d6a9836bf5468d66707c15b607b348
alesharik <alesharikReserv@yandex.ru>
2018-07-12 21:26:31 +0300

    Fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 apt-get update -qq
 apt-get install --allow-unauthenticated -qq curl openjdk-8-jdk gcc
-apt-get install --allow-unauthenticated -qq util-linux libblkid1 libblkid-dev systemd libsystemd-daemon-dev libsystemd-login-dev libsystemd-journal-dev libdbus-1-dev libsystemd-dev
+apt-get install --allow-unauthenticated -qq util-linux libblkid1 libblkid-dev systemd libsystemd-daemon-dev libsystemd-login-dev libsystemd-journal-dev libdbus-1-dev libsystemd-dev libdbus-dev
 export CXX="clang++-3.4"
 
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain 1.27.0 -y
@@ -10,5 +11,3 @@ export PATH=$HOME/.cargo/bin:$PATH
 cd /home
 chmod +x gradlew
 ./gradlew codeCoverageReport --console=plain
-
-bash <(curl -s https://codecov.io/bash)
